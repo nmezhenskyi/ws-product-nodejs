be88a92d9991fd8d472978318ce779a49878214f
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-10-24 12:20:36 -0400

    Client - Map Init

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -1,7 +1,12 @@
+
+
 export const Map = () => {
    return (
       <section className="container">
          <h1 className="main-heading">Map</h1>
+         <div className="content-section">
+
+         </div>
       </section>
    )
 }
