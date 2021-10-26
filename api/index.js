cf6d174fda059dd79396296bf8aa5177a545f2cf
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-10-25 22:04:29 -0400

    API - Extended events and stats routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const queryHandler = require('./middleware/query-handler')
 const rateLimiter = require('./middleware/rate-limiter')
+const notFoundHandler = require('./middleware/not-found-handler')
 const errorHandler = require('./middleware/error-handler')
 
 const app = express()
@@ -12,7 +13,7 @@ app.use(cors())
 //app.use(rateLimiter)
 
 app.get('/', (req, res) => {
-   res.send('Work Sample by nmezhenskyi')
+   res.send('Work Sample for EQ Works by nmezhenskyi')
 })
 
 app.get('/events/hourly', (req, res, next) => {
@@ -26,11 +27,36 @@ app.get('/events/hourly', (req, res, next) => {
 }, queryHandler)
 
 app.get('/events/daily', (req, res, next) => {
+   if (req.query.withPlaces && req.query.withPlaces === 'true') {
+      req.sqlQuery = `
+         SELECT date, name, SUM(events) AS events
+         FROM public.hourly_events
+            LEFT JOIN public.poi ON public.hourly_events.poi_id = public.poi.poi_id
+         GROUP BY date, name
+         ORDER BY date
+         LIMIT 7;
+      `
+   }
+   else {
+      req.sqlQuery = `
+         SELECT date, SUM(events) AS events
+         FROM public.hourly_events
+         GROUP BY date
+         ORDER BY date
+         LIMIT 7;
+      `
+   }
+   return next()
+}, queryHandler)
+
+app.get('/events/map', (req, res, next) => {
    req.sqlQuery = `
-      SELECT date, SUM(events) AS events
+      SELECT public.hourly_events.poi_id, name,
+         SUM(events) AS events, lat, lon
       FROM public.hourly_events
-      GROUP BY date
-      ORDER BY date
+         LEFT JOIN public.poi ON public.hourly_events.poi_id = public.poi.poi_id
+      GROUP BY public.hourly_events.poi_id, name, lat, lon
+      ORDER By public.hourly_events.poi_id
       LIMIT 7;
    `
    return next()
@@ -47,29 +73,31 @@ app.get('/stats/hourly', (req, res, next) => {
 }, queryHandler)
 
 app.get('/stats/daily', (req, res, next) => {
-   req.sqlQuery = `
-      SELECT date,
-         SUM(impressions) AS impressions,
-         SUM(clicks) AS clicks,
-         SUM(revenue) AS revenue
-      FROM public.hourly_stats
-      GROUP BY date
-      ORDER BY date
-      LIMIT 7;
-   `
-   return next()
-}, queryHandler)
-
-app.get('/map/events', (req, res, next) => {
-   req.sqlQuery = `
-      SELECT public.hourly_events.poi_id, name,
-         SUM(events) AS events, lat, lon
-      FROM public.hourly_events
-         LEFT JOIN public.poi ON public.hourly_events.poi_id = public.poi.poi_id
-      GROUP BY public.hourly_events.poi_id, name, lat, lon
-      ORDER By public.hourly_events.poi_id
-      LIMIT 7;
-   `
+   if (req.query.withPlaces && req.query.withPlaces === 'true') {
+      req.sqlQuery = `
+         SELECT date, name,
+            SUM(impressions) AS impressions,
+            SUM(clicks) AS clicks,
+            SUM(revenue) AS revenue
+         FROM public.hourly_stats
+            LEFT JOIN public.poi ON public.hourly_stats.poi_id = public.poi.poi_id
+         GROUP BY date, name
+         ORDER BY date
+         LIMIT 7
+      `
+   }
+   else {
+      req.sqlQuery = `
+         SELECT date,
+            SUM(impressions) AS impressions,
+            SUM(clicks) AS clicks,
+            SUM(revenue) AS revenue
+         FROM public.hourly_stats
+         GROUP BY date
+         ORDER BY date
+         LIMIT 7;
+      `
+   }
    return next()
 }, queryHandler)
 
@@ -81,6 +109,7 @@ app.get('/poi', (req, res, next) => {
    return next()
 }, queryHandler)
 
+app.use(notFoundHandler)
 app.use(errorHandler)
 
 app.listen(process.env.PORT || 5555, (err) => {
--- api/middleware/error-handler.js
+++ api/middleware/error-handler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
    console.error(err)
-   return res.status(500).json({ error: 'Internal error' })
+   return res.status(500).send('Internal error')
 }
 
 module.exports = errorHandler
