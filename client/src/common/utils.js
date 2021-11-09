ac34b8109fa1433b698d87ec03d256c8f6e6a45d
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2021-11-08 20:54:38 -0500

    Client - Utilities Added toUSD, toFormatteNumber; Removed escapeStringRegExp

--- client/src/common/utils.js
+++ client/src/common/utils.js
@@ -1,10 +1,23 @@
 /**
- * Converts user input string to valid regular expression.
+ * Formats string with number to USD currency representation.
  * 
  * @param {*} str Input string.
- * @returns Regular expression.
+ * @returns Formatted string.
  */
-export const escapeStringRegExp = (str) => {
-   const specialChars = /[|\\{}()[\]^$+*?.]/g
-   return str.replace(specialChars, '\\$&')
+export const toUSD = (str) => {
+   const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+   })
+   return formatter.format(str)
+}
+
+/**
+ * Formats string with number to include commas between thousands.
+ * 
+ * @param {*} str Input string.
+ * @returns Formatted string.
+ */
+export const toFormattedNumber = (str) => {
+   return Number(str).toLocaleString('en-US')
 }
